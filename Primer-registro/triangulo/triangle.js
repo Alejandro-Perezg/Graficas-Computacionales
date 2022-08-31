d4520ea146597af635a51b826f87185d40b4f023
Alejandro-Perezg <55727945+Alejandro-Perezg@users.noreply.github.com>
2022-08-31 04:37:12 -0500

    main functions enabled

--- Primer-registro/triangulo/triangle.js
+++ Primer-registro/triangulo/triangle.js
@@ -1,16 +1,18 @@
 
-class triangle
+
+class Triangle
 {
     constructor(points)
     {
         this.vertex1 = points[0]
         this.vertex2 = points[1]
         this.vertex3 = points[2]
+        
     }
     draw()
     {
         ctx.beginPath()
-        ctx.moveTo(this.vertex1[0], this.vertex1[0])
+        ctx.moveTo(this.vertex1[0],this.vertex1[1])
         ctx.lineTo(this.vertex2[0],this.vertex2[1])
         ctx.lineTo(this.vertex3[0],this.vertex3[1])
         ctx.closePath()
@@ -24,7 +26,7 @@ function getMids(points)
 {
     let vertex1 = points[0]
     let vertex2 = points[1]
-    let vertex3 = points[3]
+    let vertex3 = points[2]
     
     midA = Array((vertex1[0] + vertex2[0])/2, (vertex1[1] + vertex2[1])/2)
     midB = Array((vertex2[0] + vertex3[0])/2, (vertex2[1] + vertex3[1])/2)
@@ -36,12 +38,12 @@ function getMids(points)
 
 function drawLevels(points,steps)
 {
-    let triangle = new triangle(points)
-    if (steps > 0) {
+    let triangle = new Triangle(points).draw()
+    if (steps > 0) { 
         let newPoints = getMids(points)
-        drawLevels([points[0],newPoints[0],newPoints[0],newPoints[2]], steps - 1)
-        drawLevels([points[1],newPoints[0],newPoints[0],newPoints[1]], steps - 1)  
-        drawLevels([points[2],newPoints[1],newPoints[0],newPoints[2]], steps - 1)          
+        drawLevels([points[0], newPoints[0],newPoints[2]], steps - 1)
+        drawLevels([points[1], newPoints[0],newPoints[1]], steps - 1)  
+        drawLevels([points[2], newPoints[1],newPoints[2]], steps - 1)          
 
     }
 }
@@ -51,7 +53,7 @@ function getSliderVal(canvas,points)
     document.getElementById("slider").oninput = function(event)
     {
         let steps = event.target.value
-        document.getElementById("valSlider").innerHTML = "Recursion Level: " + event.target.value
+        document.getElementById("sliderValue").innerHTML = "Levels: " + event.target.value
         ctx.clearRect(0,0,canvas.width,canvas.height)
         drawLevels(points,steps)
     };
@@ -59,8 +61,9 @@ function getSliderVal(canvas,points)
 
 function main()
 {
+ 
     let points = [[200,100],[100,300],[300,300]]
-    let canvas = document.getElementById("triangleCanvas")
+    let canvas = document.getElementById("htmlCanvas")
     ctx = canvas.getContext("2d")
     getSliderVal(canvas,points)
 }
